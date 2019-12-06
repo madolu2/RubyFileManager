2db188cbb2ce4688738aa4c242b813b0e23881bc
madolu2 <49994326+madolu2@users.noreply.github.com>
2019-12-07 02:58:10 +0700

    All functions complete

--- FIleManager.rb
+++ FIleManager.rb
@@ -1,37 +1,24 @@
 #List of files in directory
 #User may sort files
-require 'pathname'
+require 'fileutils'
 
 $path = Dir.pwd
 
 def ListOfFiles
-  while true
-    puts '
-  TO LEAVE ENTER "back"
-  ENTER SOURCE DIRECTORY
-  USE PRESET? y/n
-  '
-    user_choice = gets.chomp
+  puts "
+TO LEAVE ENTER '<'
+WANNA SORT IT? y/n
+  "
+  user_choice = gets.chomp
 
-    if user_choice == 'y'
-      path = $path
-    elsif user_choice == "back"
-      return nil
-    end
-
-    puts '
-  WANNA SORT IT? y/n
-    '
-    user_choice = gets.chomp
-
-    if user_choice == 'y'
-      puts Dir.entries(path).sort
-    elsif user_choice == "back"
-      return nil
-    else
-      puts Dir.entries(path)
-    end
+  if user_choice == 'y'
+    puts Dir.entries($path).sort
+  elsif user_choice == '<'
+    return nil
+  else
+    puts Dir.entries($path)
   end
+  return nil
 end
 
 def JumpTo
@@ -42,37 +29,116 @@ def JumpTo
     end
 
     puts '
-  TO LEAVE ENTER "back"
+  TO LEAVE ENTER "<"
   ENTER SOURCE DIRECTORY
-  USE PRESET? y/n
+  JUMP TO ROOT FOLDER? y/n
   '
     user_choice = gets.chomp
 
     if user_choice == 'y'
-      $path = Dir.pwd
-    elsif user_choice == "back"
+      $path = '/'
+    elsif user_choice == "<"
       return nil
     else
-      $path = user_choice
+      $path = user_choice + '/'
     end
-
   end
 end
 
 def DeleteByName
+  puts '
+TO LEAVE ENTER "<"
+ENTER FILE NAME
 
+'
+  file_name = gets.chomp
+
+  if file_name == "<"
+    return nil
+  end
+
+  user_choice = $path + '/' + file_name
+  begin
+   File.delete(user_choice)
+   puts '
+DELETED
+'
+  rescue
+    puts '
+!ERROR - WRITE VALID FILE NAME!
+'
+  end
 end
 
 def DeleteByMask
+  puts '
+TO LEAVE ENTER "<"
+ENTER MASK
 
+'
+  user_choice = gets.chomp
+
+  if user_choice == "<"
+    return nil
+  end
+
+  destination_dir = $path + '/' + user_choice
+
+  File.delete(*Dir.glob(destination_dir))
+  puts '
+DELETED
+'
 end
 
 def CreateFile
+  puts "
+TO LEAVE ENTER '<'
+ENTER FILE NAME
+"
+  user_choice = gets.chomp
 
+  if user_choice == "<"
+    return nil
+  end
+
+  if user_choice != nil
+    File.open(user_choice, 'w')
+    puts "
+FILE #{user_choice} WAS CREATED
+"
+  end
+  return nil
 end
 
 def CopyFile
+  puts "
+TO LEAVE ENTER '<'
+ENTER FILE NAME
+  "
 
+  file_name = gets.chomp
+  user_choice = $path + '/' + file_name
+
+  puts "
+ENTER DESTINATION DIR
+STAY '#{$path}'? y/n
+  "
+
+  destination_dir = gets.chomp
+
+  if destination_dir == 'y'
+    destination_dir = $path + '/' + file_name + 1.to_s
+  else
+    destination_dir += file_name
+  end
+
+  File.open(destination_dir, 'w')
+
+  File.foreach(user_choice) do |line|
+    File.foreach(destination_dir) do |dline|
+      dline = line
+    end
+  end
 end
 
 puts 'HELLO THERE
@@ -80,7 +146,7 @@ IT`S FILE MANAGER'
 
 while true
   puts "
-  NOW U IN'#{$path}'"
+  NOW U IN '#{$path}'"
   puts '
   WHAT WE GONNA DO?
   1.LIST OF FILES IN DIR
